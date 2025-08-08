e3d06b637c0044e9dcb6d002d9a80b5490f6b9d4
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-08-08 15:21:36 +0100

    Diagram shield fixes.
    There are a few bits changed here
    
    * Add eventbridge connection for slack notification
    * Add eventbridge rule for alarm notifications
    * Remove encryption key and SNS topic
    * Pass route53 and cloudfront arns to the shield protection resource
    * Update the terraform policy to allow this

--- site/shield.tf
+++ site/shield.tf
@@ -1,9 +1,9 @@
 locals {
-  shield_topic_name = "${local.environment}-DiAGRAM-shield-notifications"
+  general_notifications_channel_id = local.environment == "live" ? "C06E20AR65V" : "C068RLCPZFE"
 }
 
 resource "aws_shield_protection" "shield_protection" {
-  for_each     = toset([aws_s3_bucket.website.arn, aws_iam_role.cloudwatch.arn])
+  for_each     = toset(["arn:aws:route53:::hostedzone/${var.hosted_zone_id}", aws_cloudfront_distribution.diagram.arn])
   name         = "DIAGRAMShieldProtection"
   resource_arn = each.value
 }
@@ -11,7 +11,7 @@ resource "aws_shield_protection" "shield_protection" {
 module "shield_response_team_role" {
   source             = "git::https://github.com/nationalarchives/da-terraform-modules//iam_role"
   assume_role_policy = templatefile("${path.module}/templates/iam_role/shield_response_assume_role.json.tpl", {})
-  name               = "DR2ShieldResponseTeamRole${title(local.environment)}"
+  name               = "${local.environment}-shield-team-response-role"
   policy_attachments = {
     access_policy = "arn:aws:iam::aws:policy/service-role/AWSShieldDRTAccessPolicy"
   }
@@ -19,23 +19,31 @@ module "shield_response_team_role" {
 }
 
 module "shield_response_s3_bucket" {
-  source      = "git::https://github.com/nationalarchives/da-terraform-modules//s3"
-  common_tags = local.common_tags
-  bucket_name = aws_s3_bucket.website.bucket
+  source            = "git::https://github.com/nationalarchives/da-terraform-modules//s3"
+  common_tags       = local.common_tags
+  bucket_name       = "${local.environment}-diagram-shield-response"
+  create_log_bucket = false
+
 }
 
-module "encryption_key" {
-  source     = "git::https://github.com/nationalarchives/da-terraform-modules//kms"
-  key_name   = "${local.environment}-kms-DiAGRAM"
-  key_policy = "message_system_access"
-}
-
-module "notifications_topic" {
-  source      = "git::https://github.com/nationalarchives/da-terraform-modules//sns"
-  topic_name  = local.shield_topic_name
-  sns_policy  = "notifications"
-  kms_key_arn = module.encryption_key.kms_key_arn
-  tags        = {}
+module "cloudwatch_event_alarm_event_bridge_rule_alarm_only" {
+  source = "git::https://github.com/nationalarchives/da-terraform-modules//eventbridge_api_destination_rule"
+  event_pattern = templatefile("${path.module}/templates/eventbridge/cloudwatch_alarm_event_pattern.json.tpl", {
+    cloudwatch_alarms = jsonencode([module.shield_cloudwatch_rules["s3_bucket"].cloudwatch_alarm_arn, module.shield_cloudwatch_rules["cloudfront"].cloudwatch_alarm_arn]),
+    state_value       = "ALARM"
+  })
+  name                = "${local.environment}-dr2-eventbridge-alarm-state-change-alarm-only"
+  api_destination_arn = var.api_destination_arn
+  api_destination_input_transformer = {
+    input_paths = {
+      "alarmName"    = "$.detail.alarmName",
+      "currentValue" = "$.detail.state.value"
+    }
+    input_template = templatefile("${path.module}/templates/eventbridge/slack_message_input_template.json.tpl", {
+      channel_id   = local.general_notifications_channel_id
+      slackMessage = ":warning: Cloudwatch alarm <alarmName> has entered state <currentValue>"
+    })
+  }
 }
 
 module "shield_cloudwatch_rules" {
@@ -47,7 +55,6 @@ module "shield_cloudwatch_rules" {
   name                = "shield-metric-${each.key}"
   metric_name         = "DDoSDetected"
   threshold           = 1
-  notification_topic  = module.notifications_topic.sns_arn
   dimensions          = { "ResourceArn" = each.value }
   statistic           = "Sum"
   namespace           = "AWS/DDoSProtection"
--- site/variables.tf
+++ site/variables.tf
@@ -3,3 +3,5 @@ variable "hosted_zone_id" {}
 variable "hosted_zone_name" {}
 
 variable "created_by" {}
+
+variable "api_destination_arn" {}
